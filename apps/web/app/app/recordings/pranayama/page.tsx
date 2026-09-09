8f27c8b0737e4a169a54a5ab47ed15a6dcb2719f
rutviq-ux <rutviq@mukhamudra.com>
2026-09-09 16:45:44 +0530

    Add Pranayama recordings page component

---
+++ apps/web/app/app/recordings/pranayama/page.tsx
@@ -0,0 +1,11 @@
+import { ProgramRecordings } from "../program-recordings";
+
+export default function PranayamaRecordingsPage() {
+  return (
+    <ProgramRecordings
+      program="Pranayama"
+      title="Pranayama Recordings"
+      subtitle="Revisit your breathwork sessions anytime"
+    />
+  );
+}
