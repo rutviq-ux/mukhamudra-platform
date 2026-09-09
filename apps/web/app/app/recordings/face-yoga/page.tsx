2c7ba0263552b76893e6a9b28aa6ada447442243
rutviq-ux <rutviq@mukhamudra.com>
2026-09-09 16:46:12 +0530

    Add Face Yoga Recordings page component

---
+++ apps/web/app/app/recordings/face-yoga/page.tsx
@@ -0,0 +1,11 @@
+import { ProgramRecordings } from "../program-recordings";
+
+export default function FaceYogaRecordingsPage() {
+  return (
+    <ProgramRecordings
+      program="Face Yoga"
+      title="Face Yoga Recordings"
+      subtitle="Revisit your face yoga sessions anytime"
+    />
+  );
+}
